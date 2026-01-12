b422980bc8db63d8690c0b2d9416bda3cc6d024d
Niko Covic <niko.covic@gmail.com>
2026-01-12 19:50:28 +0100

    Reworked visualize.py

--- visualize.py
+++ visualize.py
@@ -1,25 +1,46 @@
 import json
 import matplotlib.pyplot as plt
 import tyro
+from typing import Set, Literal
 
 
-def visualize(experiment:str):
-    results = json.load(open(experiment, "r"))
-    trackers = results["trackers"]
+def visualize(experiment:str,
+              metrics:Set[Literal["sharpness", "spectral_norm", "eff_sharpness", "eff_spectral_norm", "update_sharpness", "update_spectral_norm",
+                                    "train_loss_history", "val_loss_history", "train_acc_history", "val_acc_history", "all"]]={"all"},
+              cols:int=3):
+    
+    results = json.load(open(f"experiments/{experiment}/results.json", "r"))
+    
+    metrics = list(metrics)
+    if "all" in metrics:
+        metrics = results["trackers"] + ["train_loss_history", "val_loss_history", "train_acc_history", "val_acc_history"]
 
-    n_cols = min(3, len(trackers))
-    n_rows = len(trackers) // 3 + 1 + (len(trackers) < 3)
+    print(metrics)
+    
+    loss_acc_metrics = ["train_loss_history", "val_loss_history", "train_acc_history", "val_acc_history"]
+    tracker_metrics = results["trackers"]
+
+    n_cols = min(cols, len(metrics))
+    n_rows = len(metrics) // cols + (len(metrics) < cols) + 1
 
     fig, ax = plt.subplots(nrows=n_rows, ncols=n_cols, squeeze=False)
 
-    for idx, tracker in enumerate(trackers):
-        n_warmup = results[tracker]["n_warmup"]
-        freq = results[tracker]["freq"]
-        measurements = results[tracker]["measurements"]
+    for idx, metric in enumerate(metrics):
+        if metric in loss_acc_metrics:
+            measurements = results[metric]
+            n_warmup = 0
+            freq = 1
+        elif metric in tracker_metrics:
+            measurements = results[metric]["measurements"]
+            n_warmup = results[metric]["n_warmup"]
+            freq = results[metric]["freq"]
+        else:
+            raise Exception(f"Metric {metric} was not computed in this experiment.")
+
         x = [n_warmup + i*freq for i in range(len(measurements))]
-        i = idx//3 + 1
-        j = idx % 3
-        ax[i, j].plot(x, measurements, label=tracker)
+        i = idx//cols
+        j = idx % cols
+        ax[i, j].plot(x, measurements, label=metric)
         ax[i, j].legend()
     plt.show()
 
